2590a9cfced5c1ec1d8909136971c0eaac502ca1
moon9ua <koom6@naver.com>
2022-01-23 11:16:55 +0900

    chore: eslint 설정 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-      <div>It's Navigation.</div>
+      <div>It&apos;s Navigation.</div>
+      {/* <div>It's Navigation.</div> */}
+      {/* NOTE: "'"를 &apos; 등으로 대치하라고 eslint에서 에러 */}
 
       <Routes>
-        <Route path="/" element={<div>It's Main!</div>} />
-        <Route path="/page1/*" element={<div>It's Page1.</div>} />
-        <Route path="/page2/*" element={<div>It's Page2.</div>} />
+        <Route path="/" element={<div>It&apos;s Main!</div>} />
+        <Route path="/page1/*" element={<div>It&apos;s Page1.</div>} />
+        <Route path="/page2/*" element={<div>It&apos;s Page2.</div>} />
       </Routes>
 
-      <div>It's Footer.</div>
+      <div>It&apos;s Footer.</div>
     </BrowserRouter>
   );
 }

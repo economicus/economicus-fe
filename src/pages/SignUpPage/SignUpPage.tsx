6d13bb4ba42ff34d891660cf0c16a3a3441a57bb
Jeon Youngho <42.4.youjeon@gmail.com>
2022-03-11 17:41:23 +0900

    feat: 회원가입 시도시 오류에 따라 적절한 에러메세지를 스낵바로 보여주며 해당 부분으로 포커스 이동

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,3 +1,4 @@
+import { Alert, Snackbar } from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Checkbox from "@mui/material/Checkbox";
@@ -25,6 +26,24 @@ const EconomicusLogo = styled("img")({
 
 export default function SignUpPage() {
   const navigate = useNavigate();
+  const [open, setOpen] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState<string>("");
+  const [emailError, setEmailError] = React.useState(false);
+  const [nicknameError, setNicknameError] = React.useState(false);
+  const emailRef = React.useRef<HTMLDivElement>(null);
+  const nicknameRef = React.useRef<HTMLDivElement>(null);
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // 라우터가 정상적으로 동작할 수 있도록 기본 이벤트를 막는다.(버튼을 눌렀을때 팝업이 뜨면서 창이 새로고침되는것을 막도록)
@@ -38,7 +57,17 @@ export default function SignUpPage() {
     // });
     // 데이터를 정상적으로 가져왔는지 확인하기위해 콘솔에 데이터를 찍어본다.
 
-    // TODO: 이메일 유효성 검사해서 정상적인 이메일인지 확인.
+    const emailRegex =
+      /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    const emailCurrent = data.get("email") as string;
+    if (!emailRegex.test(emailCurrent)) {
+      setErrorMessage("이메일 형식이 올바르지 않습니다.");
+      setOpen(true);
+      emailRef.current?.focus();
+      setEmailError(true);
+      return;
+    }
+    // 이메일 유효성 검사해서 정상적인 이메일인지 확인. 틀릴시 스낵바에 에러메세지 띄우고 포커스 이동
 
     const code = await signUp(
       data.get("email") as string,
@@ -49,19 +78,26 @@ export default function SignUpPage() {
     // console.log(code);
 
     if (code == 201) {
-      // 201일때 로그인 페이지로 이동(+ 이메일 정보 가져와서 로그인 정보에 미리 들어가있게 설정)
       alert("회원가입에 성공하였습니다!");
       navigate("/LoginPage", { state: data.get("email") });
     } else if (code == "duplicated nickname") {
-      // TODO: 400일때 에러 메세지에 따라 해당 부분으로 커서 이동 및 빨간색으로 강조
+      setErrorMessage("이미 사용중인 닉네임입니다.");
+      setOpen(true);
+      nicknameRef.current?.focus();
+      setNicknameError(true);
       // alert("test duplicated nickname");
     } else if (code == "duplicated email") {
+      setErrorMessage("이미 가입된 이메일입니다.");
+      setOpen(true);
+      emailRef.current?.focus();
+      setEmailError(true);
       // alert("test duplicated email");
     } else {
-      // alert("server error");
+      alert("알 수 없는 오류입니다. 서버에 이상이 있을수있으니 문의 바랍니다");
     }
-
     // 회원가입 함수에서 가져온 상태에 따라 적절하게 분기한다
+    // 201일때 로그인 페이지로 이동(+ 이메일 정보 가져와서 로그인 정보에 미리 들어가있게 설정)
+    // 400일때 에러 메세지에 따라 해당 부분으로 커서 이동 및 빨간색으로 강조 + 스낵바 띄우기
   };
 
   return (
@@ -96,6 +132,8 @@ export default function SignUpPage() {
                   name="email"
                   autoComplete="email"
                   autoFocus
+                  inputRef={emailRef}
+                  error={emailError}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -117,6 +155,8 @@ export default function SignUpPage() {
                   fullWidth
                   id="nickname"
                   label="닉네임"
+                  inputRef={nicknameRef}
+                  error={nicknameError}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -144,6 +184,11 @@ export default function SignUpPage() {
           </Box>
         </Box>
       </Container>
+      <Snackbar open={open} autoHideDuration={10000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          {errorMessage}
+        </Alert>
+      </Snackbar>
     </ThemeProvider>
   );
 }
